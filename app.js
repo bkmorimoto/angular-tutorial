db8c1e4d0541399e7cef0c23b0f3b2fc48ccc7b0
Ben Morimoto <bkmorimoto@gmail.com>
2015-09-05 16:07:20 -0400

    adding new posts

--- app.js
+++ app.js
@@ -2,5 +2,17 @@ angular.module('flapperNews', [])
 .controller('MainCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!';
+  $scope.posts = [
+    {title: 'post 1', upvotes: 4},
+    {title: 'post 2', upvotes: 32},
+    {title: 'post 3', upvotes: 8},
+    {title: 'post 4', upvotes: 15},
+    {title: 'post 5', upvotes: 23}
+    ],
+  $scope.addPost = function() {
+    if(!$scope.title || $scope.title === '') { return; }
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
+  };
 }]);
+
