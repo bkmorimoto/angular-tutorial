c83ffdcb35832fddee9d55ce2069da57352d117d
Ben Morimoto <bkmorimoto@gmail.com>
2015-09-08 15:06:04 -0400

    add commenting on posts

--- app.js
+++ app.js
@@ -26,22 +26,46 @@ function($stateProvider, $urlRouterProvider) {
   return o;
 }])
 .controller('MainCtrl', [
-'$scope',
-'posts',
-function($scope, posts){
-  $scope.posts = posts.posts,
-  $scope.addPost = function() {
-    if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({
-      title: $scope.title, 
-      link: $scope.link,
-      upvotes: 0
-    });
-    $scope.title = '';
-    $scope.link = '';
-  },
-  $scope.incrementUpvotes = function(post) {
-    post.upvotes += 1;
+  '$scope',
+  'posts',
+  function($scope, posts){
+    $scope.posts = posts.posts,
+    $scope.addPost = function() {
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title, 
+        link: $scope.link,
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
+      });
+      $scope.title = '';
+      $scope.link = '';
+    },
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    }
   }
-}]);
+])
+.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id],
+    $scope.addComment = function() {
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: $scope.author,
+        upvotes: 0
+      });
+    },
+    $scope.incrementUpvotes = function(comment) {
+      comment.upvotes += 1;
+    }
+  }
+]);
 
