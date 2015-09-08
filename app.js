cee12e88b2ba3cb06e523313c3cbcf851ca76ac8
Ben Morimoto <bkmorimoto@gmail.com>
2015-09-08 14:19:30 -0400

    create templates for the views

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ function($stateProvider, $urlRouterProvider) {
       templateUrl: '/home.html',
       controller: 'MainCtrl'
     });
+  $stateProvider
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
 
   $urlRouterProvider.otherwise('home');
 }])
